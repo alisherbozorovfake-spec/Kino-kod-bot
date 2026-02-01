88d2a94d660c1390e68b3181718bc255c77c2030
alisherbozorovfake-spec <alisherbozorovfake@gmail.com>
2026-02-01 17:51:43 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+TOKEN = "8592895853:AAEg_fZI-PozBZT6av8fZMsxbgbOYwv94mA"
+
+# Majburiy obuna kanallari (5 ta)
+CHANNELS = [
+    "@kanal1bek",
+    "@kanal2bek",
+    "@kanal3bek",
+    "@kanal4bek",
+    "@kanal5bek"
+]
+
+# Kino tashlanadigan kanal
+MOVIE_CHANNEL = "@kodkanalbek"
+
+# Kino kodlari (key: kod, value: telegram post ID)
+MOVIES = {
+    "541": 10,
+    "100": 11,
+    "777": 12
+}
